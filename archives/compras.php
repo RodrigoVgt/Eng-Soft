bad0f42ee58a024980e0a6b0605057a8ec0396ff
RodrigoVgt <rodrigo.vgt@gmail.com>
2021-10-14 23:38:45 -0300

    Arquivos modificados
    Vários arquivos foram modificados em virtude de fazer os botões funcionarem com POST, e também a fim de testar os arquivos php, que nao estavam rodando em um servidor. As modificações etão sendo feitas em uma pasta diferente de onde o trabalho se iniciou, por isso as modificações devem ficar mais esparsas.
    PS: descrever bem as mudanças pelo motivo acima

--- archives/compras.php
+++ archives/compras.php
@@ -2,10 +2,10 @@
     if(isset($_POST['produto'])) {
         echo 'Produto: ', $_POST['produto'], "  ";
     }else echo 'fail';  
-    if(isset($_POST['cliente'])) {
-        echo 'Cliente: ', $_POST['cliente'], "  ";
+    if(isset($_POST['fornecedor'])) {
+        echo 'Fornecedor: ', $_POST['fornecedor'], "  ";
     }else echo 'fail';
     if(isset($_POST['quantidade'])) {
-        echo 'Quantidade: ', $_POST['quantidade'], "  ";
+        echo 'quantidade: ', $_POST['quantidade'], "  ";
     }else echo 'fail';
 ?>
