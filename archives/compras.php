23ed1613cf6f7e5cdfc4aebe2bd87c1cc045f09d
RodrigoVgt <rodrigo.vgt@gmail.com>
2021-10-15 00:01:47 -0300

    Inserção de quebras de linha
    inserção de quebras de linha no arquivo de PHP

--- archives/compras.php
+++ archives/compras.php
@@ -1,11 +1,11 @@
 <?php  
     if(isset($_POST['produto'])) {
-        echo 'Produto: ', $_POST['produto'], "  ";
+        echo 'Produto: ', $_POST['produto'], " <br /> ";
     }else echo 'fail';  
     if(isset($_POST['fornecedor'])) {
-        echo 'Fornecedor: ', $_POST['fornecedor'], "  ";
+        echo 'Fornecedor: ', $_POST['fornecedor'], " <br />";
     }else echo 'fail';
     if(isset($_POST['quantidade'])) {
-        echo 'quantidade: ', $_POST['quantidade'], "  ";
+        echo 'Quantidade: ', $_POST['quantidade'], " <br /> ";
     }else echo 'fail';
 ?>
--- archives/vendas.php
+++ archives/vendas.php
@@ -1,11 +1,11 @@
 <?php  
     if(isset($_POST['produto'])) {
-        echo 'Produto: ', $_POST['produto'], "  ";
+        echo 'Produto: ', $_POST['produto'], " <br />";
     }else echo 'fail';  
     if(isset($_POST['cliente'])) {
-        echo 'Cliente: ', $_POST['cliente'], "  ";
+        echo 'Cliente: ', $_POST['cliente'], " <br />";
     }else echo 'fail';
     if(isset($_POST['quantidade'])) {
-        echo 'Quantidade: ', $_POST['quantidade'], "  ";
+        echo 'Quantidade: ', $_POST['quantidade'], " <br /> ";
     }else echo 'fail';
 ?>
