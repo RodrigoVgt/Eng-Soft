b276f972586601ce1ba1271bce7976de4b5c72c4
RodrigoVgt <rodrigo.vgt@gmail.com>
2021-10-15 17:38:44 -0300

    Configuração do banco
    configuração do banco para teste de variáveis e inserções (!funcionando!)

--- archives/vendas.php
+++ archives/vendas.php
@@ -1,4 +1,15 @@
 <?php  
+
+    $conn = mysqli_connect("localhost", "root", "rodrigo", "financas");
+            
+    // Check connection
+    if($conn === false){
+        die("ERROR: Could not connect. " 
+            . mysqli_connect_error());
+    }else{
+        echo 'Connections sucessful <br />';
+    }
+
     if(isset($_POST['produto'])) {
         echo 'Produto: ', $_POST['produto'], " <br />";
     }else echo 'fail';  
@@ -8,4 +19,20 @@
     if(isset($_POST['quantidade'])) {
         echo 'Quantidade: ', $_POST['quantidade'], " <br /> ";
     }else echo 'fail';
+
+    $cliente = $_REQUEST['cliente'];
+    $produto = $_REQUEST['produto'];
+    $quantidade = $_REQUEST['quantidade'];
+
+    $sql = "INSERT INTO financas (acao, produto, quant, cliFor)
+            VALUES ('Venda', '$produto', '$quantidade', '$cliente')";
+
+    if(mysqli_query($conn, $sql)){
+        echo "<h3>data stored in a database successfully." 
+        . " Please browse your localhost php my admin" 
+        . " to view the updated data</h3>"; 
+    } else{
+        echo "ERROR: Hush! Sorry $sql. " 
+            . mysqli_error($conn);
+    }
 ?>
